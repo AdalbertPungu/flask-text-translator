fe742cbf51dfb9b6b2d8c0bbd31261025c9678f6
Adalbert Pungu <18pw419@esisalama.org>
2022-02-05 00:01:14 +0200

    ajouter du code pour appeler le service

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import requests, os, uuid, json
+from dotenv import load_dotenv
+load_dotenv()
+
 from flask import Flask, redirect, url_for, request, render_template, session
 
 app = Flask(__name__)
@@ -5,3 +9,4 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
